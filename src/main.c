bf250080afffcc1d3d5baefb76b20dc3f40ef24b
Jens Pitkanen <jens@neon.moe>
2019-06-22 17:53:28 +0300

    Add sdf renderer, movement

---
+++ src/main.c
@@ -0,0 +1,120 @@
+#include "raylib.h"
+#include "raymath.h"
+
+const int VIRTUAL_SCREEN_HEIGHT = 225;
+
+Rectangle GetRenderSrc(int screenWidth, int screenHeight);
+Rectangle GetRenderDest(int screenWidth, int screenHeight);
+
+int main(void) {
+    float cameraPosition[] = { 0.0, 1.75, 0.0 };
+    float cameraRotation[] = { 0.0, 0.0, 0.0 };
+
+    SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_RESIZABLE);
+    InitWindow(800, 450, "HEL Underground");
+
+    // TODO: For Raspberry Pi compatibility, add OpenGL ES version of the shader
+    // (it's probably enough to just load the shaders and switch
+    // around the #version to "100 es")
+
+    // TODO: Add an error message state for missing shader file
+    // ("you seem to be missing sdf_shader.glsl! It should've come
+    // with the download." or something)
+
+    Shader sdfShader = LoadShader(0, "sdf_shader.glsl");
+    int resolutionLocation = GetShaderLocation(sdfShader, "resolution");
+    int cameraPositionLocation = GetShaderLocation(sdfShader, "cameraPosition");
+    int cameraRotationLocation = GetShaderLocation(sdfShader, "cameraRotation");
+
+    RenderTexture2D targetTex = LoadRenderTexture(VIRTUAL_SCREEN_HEIGHT * 2,
+                                                  VIRTUAL_SCREEN_HEIGHT);
+    float resolution[] = { VIRTUAL_SCREEN_HEIGHT * 2, VIRTUAL_SCREEN_HEIGHT };
+    SetShaderValue(sdfShader, resolutionLocation, resolution, UNIFORM_VEC2);
+
+    float lastTime = (float)GetTime();
+    while (!WindowShouldClose()) {
+        float currentTime = (float)GetTime();
+        float delta = currentTime - lastTime;
+        lastTime = currentTime;
+
+        if (IsKeyDown(KEY_LEFT)) {
+            cameraRotation[1] -= delta * 120.0;
+        }
+        if (IsKeyDown(KEY_RIGHT)) {
+            cameraRotation[1] += delta * 120.0;
+        }
+        // TODO: Camera rotation based movement
+        // Should be doable without matrix math, something like this:
+        // x = cos(r) * delta - sin(r) * delta
+        // y = sin(r) * delta + cos(r) * delta
+        if (IsKeyDown(KEY_W)) {
+            cameraPosition[2] += delta * 2.0;
+        }
+        if (IsKeyDown(KEY_A)) {
+            cameraPosition[0] -= delta * 2.0;
+        }
+        if (IsKeyDown(KEY_S)) {
+            cameraPosition[2] -= delta * 2.0;
+        }
+        if (IsKeyDown(KEY_D)) {
+            cameraPosition[0] += delta * 2.0;
+        }
+        if (IsKeyDown(KEY_LEFT_CONTROL)) {
+            cameraPosition[1] = Lerp(cameraPosition[1], 0.9, 10.0 * delta);
+        } else {
+            cameraPosition[1] = Lerp(cameraPosition[1], 1.75, 10.0 * delta);
+        }
+
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        SetShaderValue(sdfShader, cameraPositionLocation, cameraPosition, UNIFORM_VEC3);
+        SetShaderValue(sdfShader, cameraRotationLocation, cameraRotation, UNIFORM_VEC3);
+
+        BeginTextureMode(targetTex);
+        BeginShaderMode(sdfShader);
+        DrawRectangle(0, 0, VIRTUAL_SCREEN_HEIGHT * 2, VIRTUAL_SCREEN_HEIGHT,
+                      (Color){0xFF, 0x00, 0xFF, 0xFF});
+        EndShaderMode();
+        EndTextureMode();
+
+        int screenWidth = GetScreenWidth();
+        int screenHeight = GetScreenHeight();
+        DrawTexturePro(targetTex.texture,
+                       GetRenderSrc(screenWidth, screenHeight),
+                       GetRenderDest(screenWidth, screenHeight),
+                       (Vector2){0.0, 0.0}, 0.0, WHITE);
+
+        EndDrawing();
+    }
+
+    UnloadRenderTexture(targetTex);
+    UnloadTexture(targetTex.texture);
+    UnloadShader(sdfShader);
+
+    CloseWindow();
+    return 0;
+}
+
+Rectangle GetRenderSrc(int screenWidth, int screenHeight) {
+    float ratio = (float)screenWidth / (float)screenHeight;
+    if (ratio < 2.0) {
+        int margin = (VIRTUAL_SCREEN_HEIGHT * (2.0 - ratio)) / 2.0;
+        return (Rectangle){margin, 0,
+                VIRTUAL_SCREEN_HEIGHT * ratio, VIRTUAL_SCREEN_HEIGHT};
+    } else {
+        return (Rectangle){0, 0,
+                VIRTUAL_SCREEN_HEIGHT * 2, VIRTUAL_SCREEN_HEIGHT};
+    }
+}
+
+Rectangle GetRenderDest(int screenWidth, int screenHeight) {
+    float ratio = (float)screenWidth / (float)screenHeight;
+    if (ratio < 2.0) {
+        return (Rectangle){0, 0, screenWidth, screenHeight};
+    } else {
+        int margin = (screenWidth - screenHeight * 2) / 2;
+        return (Rectangle){margin, 0,
+                screenHeight * 2, screenHeight};
+    }
+}
---
+++ src/sdf_shader.glsl
@@ -0,0 +1,145 @@
+#version 120
+
+// Ray marching variables (affect performance a lot)
+#define RAY_STEPS_MAX 100
+#define SDF_SURFACE_THRESHOLD 0.005
+
+// 3D environment defining variables
+#define LIGHTS_COUNT 2
+
+varying vec4 position;
+
+uniform vec2 resolution;
+uniform vec3 cameraPosition;
+uniform vec3 cameraRotation;
+
+struct Camera {
+    vec3 position;
+    vec3 direction;
+};
+
+struct Light {
+    vec3 position;
+    float distance;
+};
+
+uniform Light lights[LIGHTS_COUNT] =
+    Light[LIGHTS_COUNT](Light(vec3(3.0, 3.0, 4.0), 3.0),
+                        Light(vec3(-4.0, 2.5, 6.0), 1.0));
+
+/* SDFs: https://www.iquilezles.org/www/articles/distfunctions/distfunctions.htm */
+
+float sdfSphere(vec3 samplePos, vec3 position, float radius) {
+    return length(position - samplePos) - radius;
+}
+
+float sdfBox(vec3 samplePos, vec3 center, vec3 extents) {
+  vec3 d = abs(center - samplePos) - extents;
+  return length(max(d, 0.0)) + min(max(d.x, max(d.y, d.z)), 0.0);
+}
+
+float sdf(vec3 samplePos) {
+    return min(
+        sdfSphere(samplePos, vec3(0.0, 1.0, 5.0), 0.5),
+        sdfBox(samplePos, vec3(0.0, -0.5, 5.0), vec3(5.0, 1.0, 5.0)));
+}
+
+vec4 rotate_x(vec4 direction, float degrees) {
+    float r = radians(degrees);
+    mat4 transform = mat4(vec4(1.0, 0.0, 0.0, 0.0),
+                          vec4(0.0, cos(r), sin(r), 0.0),
+                          vec4(0.0, -sin(r), cos(r), 0.0),
+                          vec4(0.0, 0.0, 0.0, 1.0));
+    return transform * direction;
+}
+
+vec4 rotate_y(vec4 direction, float degrees) {
+    float r = radians(degrees);
+    mat4 transform = mat4(vec4(cos(r), 0.0, -sin(r), 0.0),
+                          vec4(0.0, 1.0, 0.0, 0.0),
+                          vec4(sin(r), 0.0, cos(r), 0.0),
+                          vec4(0.0, 0.0, 0.0, 1.0));
+    return transform * direction;
+}
+
+float get_fog(vec3 cam, vec3 position) {
+    return min(1.0, pow(5.0 / length(cam - position), 1.5));
+}
+
+vec3 get_normal(vec3 samplePos) {
+    float x = sdf(samplePos + vec3(SDF_SURFACE_THRESHOLD, 0.0, 0.0)) -
+        sdf(samplePos - vec3(SDF_SURFACE_THRESHOLD, 0.0, 0.0));
+    float y = sdf(samplePos + vec3(0.0, SDF_SURFACE_THRESHOLD, 0.0)) -
+        sdf(samplePos - vec3(0.0, SDF_SURFACE_THRESHOLD, 0.0));
+    float z = sdf(samplePos + vec3(0.0, 0.0, SDF_SURFACE_THRESHOLD)) -
+        sdf(samplePos - vec3(0.0, 0.0, SDF_SURFACE_THRESHOLD));
+    return normalize(vec3(x, y, z));
+}
+
+float get_brightness(vec3 samplePos, vec3 normal, float fog) {
+    float ambient = 0.1;
+    float diffuse = 0.0;
+    for (int i = 0; i < LIGHTS_COUNT; i++) {
+        Light light = lights[i];
+        vec3 lightDir = light.position - samplePos;
+        diffuse += light.distance / length(lightDir) *
+            max(0.0, dot(normal, normalize(lightDir))) * 0.2;
+    }
+    return min(1.0, diffuse) + ambient;
+}
+
+float get_ambient_occlusion(vec3 samplePos, vec3 normal) {
+    vec3 step = normal * 0.25;
+    float distance = 0.1;
+    for (; distance < 1.0; distance += 0.1) {
+        if (sdf(samplePos + step * distance) < SDF_SURFACE_THRESHOLD) {
+            break;
+        }
+    }
+    return 1.1 - distance;
+}
+
+vec4 get_color(vec2 screenPosition, vec3 position, vec3 rotation) {
+    // TODO: Configurable FoV
+    float nearDistance = 0.5; // This is 90 degrees FoV
+    vec3 direction = vec3(screenPosition.x, screenPosition.y, nearDistance);
+    direction = normalize(direction);
+    direction = rotate_x(vec4(direction, 1.0), rotation.x).xyz;
+    direction = rotate_y(vec4(direction, 1.0), rotation.y).xyz;
+
+    vec3 originalPosition = position;
+    bool hit = false;
+    vec3 normal = vec3(0.0, 0.0, 0.0);
+    int steps = 1;
+    for (; steps < RAY_STEPS_MAX; steps++) {
+        float distance = sdf(position);
+        if (distance < SDF_SURFACE_THRESHOLD) {
+            hit = true;
+            normal = get_normal(position);
+            break;
+        } else {
+            position += direction * distance;
+        }
+    }
+
+    if (hit) {
+        float fog = get_fog(originalPosition, position);
+        float brightness = get_brightness(position, normal, fog);
+        float ambientOcclusion = pow(get_ambient_occlusion(position, normal), 4.0) * 0.2;
+        return vec4(vec3(1.0, 1.0, 1.0) * brightness * fog - ambientOcclusion, 1.0);
+    } else {
+        return vec4(0.0, 0.0, 0.0, 1.0);
+    }
+}
+
+void main() {
+    // TODO: Ensure HiDPI compatibility
+
+    // NOTE: The y coordinate is flipped because we're rendering to a render texture
+    vec2 pixelCoords = vec2((gl_FragCoord.x - resolution.x / 2.0) / resolution.y,
+                            (gl_FragCoord.y / resolution.y - 0.5) * -1.0);
+
+    // TODO: Switch to render textures for lowering the resolution
+
+    gl_FragColor = get_color(pixelCoords, cameraPosition, cameraRotation);
+}
